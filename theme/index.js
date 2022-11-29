bea914c6185b7fe723da991d3f670296ed1df732
Pablo Lara <larabjj@gmail.com>
2022-11-29 07:49:02 +0100

    style: change box-shadow for Cards

--- theme/index.js
+++ theme/index.js
@@ -60,6 +60,13 @@ export const theme = createTheme({
         disableRipple: true,
       },
     },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          boxShadow: 'rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px'
+        },
+      },
+    },
     MuiCardContent: {
       styleOverrides: {
         root: {

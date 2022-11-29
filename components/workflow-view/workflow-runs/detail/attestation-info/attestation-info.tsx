f7832da9cda76e16e263581ae0383fd6fa179af0
Miguel Martinez Trivino <migmartri@gmail.com>
2022-11-29 11:10:19 +0100

    add envelope tabs

--- components/workflow-view/workflow-runs/detail/attestation-info/attestation-info.tsx
+++ components/workflow-view/workflow-runs/detail/attestation-info/attestation-info.tsx
@@ -1,23 +1,50 @@
 import { AttestationItem } from "@pb/controlplane/v1/response_messages";
-import { Grid } from "@mui/material";
+import { Box, Tabs, Tab, Divider } from "@mui/material";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { agate as theme } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { ReactNode, useState } from "react";
 
 export const AttestationInfo = ({
   attestation,
 }: {
   attestation: AttestationItem;
 }) => {
+  const [selectedTab, selectTab] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    selectTab(newValue);
+  };
+
   return (
-    <Grid container>
-      <Grid item xs={12} sx={{ fontSize: "12px" }}>
-        <SyntaxHighlighter language="json" style={theme}>
-          {JSON.stringify(statement(attestation), null, 2)}
-        </SyntaxHighlighter>
-      </Grid>
-    </Grid>
+    <Box>
+      <Tabs
+        value={selectedTab}
+        onChange={handleChange}
+        textColor="inherit"
+        indicatorColor="secondary"
+        sx={{
+          borderBottom: "1px solid",
+          borderColor: "divider",
+        }}
+      >
+        <Tab label="Statement" />
+        <Tab label="Envelope" />
+      </Tabs>
+      <TabPanel index={0} value={selectedTab}>
+        <Codehighlighter data={statement(attestation)} />
+      </TabPanel>
+      <TabPanel index={1} value={selectedTab}>
+        <Codehighlighter data={envelope(attestation)} />
+      </TabPanel>
+    </Box>
   );
 };
+
+const Codehighlighter = ({ data }: { data: any }) => (
+  <SyntaxHighlighter language="json" style={theme}>
+    {JSON.stringify(data, null, 2)}
+  </SyntaxHighlighter>
+);
 
 const envelope = (att: AttestationItem) => {
   const raw = new TextDecoder().decode(att.envelope);
@@ -30,4 +57,26 @@ const statement = (att: AttestationItem) => {
     "utf8"
   );
   return JSON.parse(statementRaw);
+};
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const TabPanel = (props: TabPanelProps) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && children}
+    </div>
+  );
 };

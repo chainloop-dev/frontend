cbf2737bebcb8c6588851c461bfcafb45bf1f618
Pablo Lara <larabjj@gmail.com>
2022-11-30 16:04:28 +0100

    style: revert tables styles by default

--- theme/index.js
+++ theme/index.js
@@ -123,6 +123,29 @@ export const theme = createTheme({
         },
       },
     },
+    MuiTableHead: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#F3F4F6",
+          ".MuiTableCell-root": {
+            color: "#374151",
+          },
+          borderBottom: "none",
+          "& .MuiTableCell-root": {
+            borderBottom: "none",
+            fontSize: "12px",
+            fontWeight: 600,
+            lineHeight: 1,
+            letterSpacing: 0.5,
+            textTransform: "uppercase",
+          },
+          "& .MuiTableCell-paddingCheckbox": {
+            paddingTop: 4,
+            paddingBottom: 4,
+          },
+        },
+      },
+    },
     MuiOutlinedInput: {
       styleOverrides: {
         notchedOutline: {

8a52b1c6c68c4479498f5ca19a3e333ad92c8f30
Miguel Martinez Trivino <migmartri@gmail.com>
2022-12-01 11:46:04 +0100

    filter

--- app/dashboard/workflow-runs/page.tsx
+++ app/dashboard/workflow-runs/page.tsx
@@ -9,6 +9,7 @@ import {
 } from "@lib/apiclient/workflowRuns";
 import { WorkflowRunsListResults } from "@components/workflow-runs-list/result/result";
 import { useEffect, useRef, useState } from "react";
+import { useWorkflows } from "@lib/apiclient/workflows";
 
 export default function WorkflowRunsList() {
   const { apiClient } = useAuth();
@@ -17,11 +18,20 @@ export default function WorkflowRunsList() {
   const mapPageToNextCursor = useRef<{ [page: number]: string }>({});
   const [limit, setLimit] = useState(25);
   const [currentPage, setCurrentPage] = useState(0);
+  const [workflowID, setWorkflowID] = useState("");
   const opts: IRunsListOpts = {
     limit: limit,
+    workflowID: workflowID,
     cursor: mapPageToNextCursor.current[currentPage - 1],
   };
-  const { isLoading, data } = useWorkflowRunsList(opts, apiClient);
+  const { isLoading: loadingRuns, data: runs } = useWorkflowRunsList(
+    opts,
+    apiClient
+  );
+
+  // Load workflows to enable filtering
+  const { isLoading: loadingWorkflows, data: workflows } =
+    useWorkflows(apiClient);
 
   const handleChangePage = (_e: any, newPage: number) => {
     // We have the cursor, we can allow the page transition.
@@ -31,11 +41,11 @@ export default function WorkflowRunsList() {
   };
 
   useEffect(() => {
-    const nc = data?.pagination?.nextCursor;
+    const nc = runs?.pagination?.nextCursor;
     if (nc) {
       mapPageToNextCursor.current[currentPage] = nc;
     }
-  }, [data?.pagination?.nextCursor]);
+  }, [runs?.pagination?.nextCursor]);
 
   const handleLimitChange = (limit: number) => {
     // Reset page to make sure that going back works
@@ -48,20 +58,23 @@ export default function WorkflowRunsList() {
   }, [limit]);
 
   return (
-    <WithLoader loading={isLoading}>
+    <WithLoader loading={loadingRuns || loadingWorkflows}>
       <Box component="main" sx={{ flexGrow: 1 }}>
         <Container maxWidth={false}>
           <Typography sx={{ m: 1 }} variant="h4">
             Workflow Runs
           </Typography>
           <Box sx={{ mt: 3 }}>
-            {data?.result && (
+            {runs?.result && workflows?.result && (
               <WorkflowRunsListResults
                 page={currentPage}
+                workflowID={workflowID}
+                setWorkflowID={setWorkflowID}
                 handlePageChange={handleChangePage}
                 limit={limit}
                 setLimit={handleLimitChange}
-                runs={data?.result}
+                runs={runs?.result}
+                workflows={workflows?.result}
               ></WorkflowRunsListResults>
             )}
           </Box>
--- components/workflow-runs-list/result/result.tsx
+++ components/workflow-runs-list/result/result.tsx
@@ -9,8 +9,17 @@ import {
   TableCell,
   TableBody,
   TablePagination,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Grid,
+  SelectChangeEvent,
 } from "@mui/material";
-import { WorkflowRunItem } from "@pb/controlplane/v1/response_messages";
+import {
+  WorkflowItem,
+  WorkflowRunItem,
+} from "@pb/controlplane/v1/response_messages";
 import { format } from "date-fns";
 
 export const WorkflowRunsListResults = ({
@@ -18,12 +27,18 @@ export const WorkflowRunsListResults = ({
   limit,
   setLimit,
   handlePageChange,
+  setWorkflowID,
   page,
+  workflowID,
+  workflows,
 }: {
   runs: WorkflowRunItem[];
+  workflows: WorkflowItem[];
+  workflowID: string;
   limit: number;
   page: number;
   setLimit: (_limit: number) => void;
+  setWorkflowID: (_wf: string) => void;
   handlePageChange: (_e: unknown, _page: number) => void;
 }) => {
   const handleChangeRowsPerPage = (
@@ -32,8 +47,31 @@ export const WorkflowRunsListResults = ({
     setLimit(parseInt(event.target.value, 10));
   };
 
+  const handleWorkflowIDChange = (event: SelectChangeEvent) => {
+    setWorkflowID(event.target.value as string);
+  };
+
   return (
     <>
+      <Grid container justifyContent="right" pb="20px">
+        <Grid item xs={12} sm={3} xl={2}>
+          <FormControl fullWidth>
+            <InputLabel>Filter by Workflow</InputLabel>
+            <Select
+              value={workflowID}
+              label="Workflow"
+              onChange={handleWorkflowIDChange}
+            >
+              <MenuItem value="">Any</MenuItem>
+              {workflows?.map((run) => (
+                <MenuItem value={run.id} key={run.id}>
+                  {run.project}/{run.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
